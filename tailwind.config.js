b530b8d30c0f7489274c2bffc8dde04a4e5b1975
lucasptcastro <lucasptcastro@gmail.com>
2023-09-03 20:21:47 -0300

    ✨ feat: added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./App.tsx"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // "./src/*.{js,jsx,ts,tsx}",
+    "./App.tsx",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: "Roboto_400Regular",
+        body: "Roboto_700Bold",
+        alt: "Nunito_400Regular",
+        // title: "Nunito_700Bold",
+        // title: "Comfortaa_400Regular",
+        // title: "Comfortaa_700Bold",
+        // title: "Poppins_400Regular",
+        // title: "Poppins_700Bold",
+      },
+    },
   },
   plugins: [],
 };
